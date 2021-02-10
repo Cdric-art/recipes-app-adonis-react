c606893940843a2155cad4c1ced24c8408abd7ad
Cdric-art <cdric.art@gmail.com>
2021-02-10 17:17:08 +0100

    Feat: Route get / for check server running

--- start/routes.ts
+++ start/routes.ts
@@ -2,6 +2,13 @@ import Route from '@ioc:Adonis/Core/Route'
 import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 /**
+ * Verify server running
+ */
+Route.get('/', async () => {
+  return 'Server Running'
+})
+
+/**
  * Verify state App
  */
 Route.get('health', async ({response}) => {
